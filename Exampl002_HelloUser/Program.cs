c57626e93c85f974174a67b26fdb32bac6b6f027
Alexander Kamaev <qalex2@yandex.ru>
2023-07-02 13:52:26 +0300

    add Exampl002_HelloUser project

---
+++ Exampl002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿//Console.WriteLine("Введите свое имя: " );
+//string username = Console.ReadLine();
+//Console.WriteLine("Привет, " );
+//Console.WriteLine(username);
+
+Console.Write("Введите свое имя: " );
+string username = Console.ReadLine();
+Console.Write("Привет, " );
+Console.Write(username);
