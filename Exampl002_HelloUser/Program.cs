5e43b0ea0747f64dafa7f3b7db17f51150420b27
QAlex68 <138191734+QAlex68@users.noreply.github.com>
2023-07-02 14:50:27 +0300

    Delete Exampl002_HelloUser directory

--- Exampl002_HelloUser/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿//Console.WriteLine("Введите свое имя: " );
-//string username = Console.ReadLine();
-//Console.WriteLine("Привет, " );
-//Console.WriteLine(username);
-
-Console.Write("Введите свое имя: " );
-string username = Console.ReadLine();
-Console.Write("Привет, " );
-Console.Write(username);
