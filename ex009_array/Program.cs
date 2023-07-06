b641889a23bc913d1887790245457e9c4fdc99c9
Alexander Kamaev <qalex2@yandex.ru>
2023-07-06 18:50:14 +0300

    массивы 2

--- ex009_array/Program.cs
+++ ex009_array/Program.cs
@@ -6,10 +6,10 @@
     return result;
 }
 //             0  1  2  3  4  5  6  7  0
-int[] array = {13, 26, 43, 34, 55, 65, 17, 18, 29};
+int[] array = { 13, 26, 43, 34, 55, 65, 17, 18, 29 };
 
 // Как вариант
-int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]),Max(array[6], array[7], array[8]));
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
 
 Console.WriteLine("Максимальный элемент равен:" + max);
 
